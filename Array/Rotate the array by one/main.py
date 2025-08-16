1fdad3acc3a9e703c00a3d219c716a532949ea85
Sagar Bankar <145119125+Sagar27044@users.noreply.github.com>
2025-08-16 21:34:53 +0530

    Create main.py

---
+++ Array/Rotate the array by one/main.py
@@ -0,0 +1,7 @@
+def solve(arr, n):
+    temp = arr[0]  # storing the first element of the array in a variable
+    for i in range(n - 1):
+        arr[i] = arr[i + 1]
+    arr[n - 1] = temp  # assign the value of the variable at the last index
+    for i in range(n):
+        print(arr[i], end=" ")
