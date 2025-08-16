698c71965757986ebbf5d6b98f9abc9553539f72
Sagar Bankar <145119125+Sagar27044@users.noreply.github.com>
2025-08-16 21:42:16 +0530

    Update main.py

--- Array/Rotate the array by one/main.py
+++ Array/Rotate the array by one/main.py
@@ -1,7 +1,9 @@
-def solve(arr, n):
-    temp = arr[0]  # storing the first element of the array in a variable
-    for i in range(n - 1):
-        arr[i] = arr[i + 1]
-    arr[n - 1] = temp  # assign the value of the variable at the last index
-    for i in range(n):
-        print(arr[i], end=" ")
+class Solution:
+    def rotate(self, arr):
+        n = len(arr)
+        if n == 0:  # handle empty array
+            return
+        temp = arr[-1]  # store the last element
+        for i in range(n-1, 0, -1):
+            arr[i] = arr[i-1]  # shift elements to the right
+        arr[0] = temp  # place last element at the start
